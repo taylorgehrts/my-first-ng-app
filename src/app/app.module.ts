fcc90c2475a3d25351d0bd824555e2e3ec7310d4
taylorgehrts <taylorgehrts@gmail.com>
2023-12-27 13:56:25 +0100

    finished adding comonent to the modules file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    ServerComponent
   ],
   imports: [
     BrowserModule
